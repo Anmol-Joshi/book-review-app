d5819ed1e8df3e868cacc2da1192a02881484d2c
Anmol Joshi <anmol01joshi@gmail.com>
2021-08-09 10:17:26 -0400

    update

--- src/index.js
+++ src/index.js
@@ -3,26 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import LoginPage from './LoginPage';
 import Profile from './Profile';
-import ProductList from './ProductList'
-import ProductDetailPage from './ProductDetailPage'
+import ProductList from './ProductList';
+import ProductDetailPage from './ProductDetailPage';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
     <Router>
-        {/* A <Switch> looks through its children <Route>s and
+      {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route exact path="/" component={LoginPage} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/productlist" component={ProductList} />
-          <Route exact path="/productdetailpage/:id" component={ProductDetailPage} />
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={LoginPage} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/productlist" component={ProductList} />
+        <Route
+          exact
+          path="/productdetailpage/:id"
+          component={ProductDetailPage}
+        />
+      </Switch>
     </Router>
   );
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
