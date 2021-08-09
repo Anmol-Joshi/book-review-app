523c0820d73e9ac638fc4227323b3c521cf98484
Anmol Joshi <anmol01joshi@gmail.com>
2021-08-09 03:17:00 -0400

    post review

--- src/Components/ProductDetailPageReview/ProductDetailPageReview.jsx
+++ src/Components/ProductDetailPageReview/ProductDetailPageReview.jsx
@@ -8,36 +8,13 @@ class ProductDetailPageReview extends React.Component {
   constructor(props) {
     super(props);
   }
-  // handleSubmit = (event) => {
-  //   const postData = {
-  //     review: this.state.review,
-  //     rating: this.state.rating,
-  //   };
-  //   if (this.state.review === '' || this.state.rating === '') {
-  //     alert('Rating/Review cannot be empty');
-  //   } else {
-  //     console.log(postData);
-  //     axios
-  //       .post(
-  //         `https://best-read.herokuapp.com/api/items/${this.props.id}/reviews`,
-  //         postData
-  //       )
-  //       .then(() => {
-  //         alert('review submitted');
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //       });
-  //   }
-  //   // alert('A review was submitted: ' + this.state.review + this.state.rating);
-  //   event.preventDefault();
-  // };
   render() {
     return (
       <div>
         product detail page review
         <div>{this.props.id}</div>
-        <PostReview />
+        <PostReview id={this.props.id} />
+        {/* <GetReviews /> */}
       </div>
     );
   }
