1def9b4d38e44bc105e688e4d9bf921318bc0c47
Anmol Joshi <anmol01joshi@gmail.com>
2021-08-22 01:49:54 -0400

    update

--- server/routes/items.js
+++ server/routes/items.js
@@ -125,7 +125,7 @@ router.post('/:itemId/reviews', auth.authenticate, (req, res) => {
         ratingSum += review.rating;
         totalRatings++;
       });
-      Item.findOne({ _id: req.params.itemId })
+      Item.findById(req.params.itemId)
         .then((item) => {
           item.totalRatings = totalRatings;
           item.ratingSum = ratingSum;
